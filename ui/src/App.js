114b32aff5cdcf26bef785730b9aa7fe8693014b
Ilya Ilyukou <Ilya_Ilyukou@epam.com>
2023-08-13 16:33:18 +0300

    Read title from env variables and apply it to the app

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,8 +2,13 @@ import {Navigate, Route, Routes} from "react-router-dom";
 
 import Home from "./pages/home/Home";
 import NotFound from "./pages/404/404";
+import {useEffect} from "react";
 
 export default function App() {
+    useEffect(() => {
+        document.title = process.env.REACT_APP_TITLE
+    }, []);
+
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
